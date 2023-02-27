e1c25d29becc792da26b61836d5a31837037fce9
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-02-27 15:34:34 +0000

    ugga jugga

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html>
   <head>
     <title>Page Title</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/images/site.webmanifest">
+
   </head>
 
   <body>
@@ -9,6 +15,12 @@
     <?php
     echo "<h1>Hello, World!</h1>\n";
     ?>
+
+  <?php
+  $txt = "Hello world!";
+  $x = 5;
+  $y = 10.5;
+  ?>
     
   </body>
   
