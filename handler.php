3ef44760c4ae0997fe8a846f2d536e148744023f
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-02-28 15:33:53 +0000

    flakjsdf;

--- handler.php
+++ handler.php
@@ -3,6 +3,14 @@
   <head>
     <link rel="stylesheet" href="styles.css">
     <title>Page Title</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/images/site.webmanifest">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-yellow.min.css" />
   </head>
 
   <body>
